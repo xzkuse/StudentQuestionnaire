0801eed1581c0322b660871ae313734118e51994
谢正坤 <xiezhengkun@beyondsoft.com>
2019-10-25 20:14:25 +0800

    增加ivew的ui库

---
+++ StudentQuestionnaire_wx/pages/evaluate/evaluate.js
@@ -0,0 +1,22 @@
+Page({
+
+	/**
+	 * 页面的初始数据
+	 */
+  data: {
+    visible1:false
+
+  },
+
+	/**
+	 * 生命周期函数--监听页面加载
+	 */
+  onLoad: function (options) { 
+  },
+  exit: function (e) { 
+  },
+  resetpwd: function (e) { 
+  },
+  setemail: function (e) { 
+  }
+})
