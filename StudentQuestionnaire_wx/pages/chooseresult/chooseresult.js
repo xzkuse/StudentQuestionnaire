b2f8ca3c28449be3c5940ad1c09acb5f706a949e
谢正坤 <xiezhengkun@beyondsoft.com>
2019-10-31 18:59:59 +0800

    增加答题结果页面

---
+++ StudentQuestionnaire_wx/pages/chooseresult/chooseresult.js
@@ -0,0 +1,77 @@
+// pages/chooseresult/chooseresult.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    questionResultArr: [
+      { name: '1', result: true },
+      { name: '2', result: true },
+      { name: '3', result: true },
+      { name: '4', result: true },
+      { name: '5', result: true },
+      { name: '1', result: false },
+      { name: '2', result: false },
+      { name: '3', result: false },
+      { name: '4', result: false },
+      { name: '5', result: false },
+    ],
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
