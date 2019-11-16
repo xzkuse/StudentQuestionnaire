87ea46f1edef6db0e663e4fdd31e6df6ae732039
谢正坤 <xiezhengkun@beyondsoft.com>
2019-11-16 16:30:28 +0800

    增加对kotlin的理解，包括，基本类，接口，扩展类函数，泛型，半生函数，协程基础等

--- androidkotlin/app/src/main/java/com/mallcloud/utils/kotenum.kt
+++ androidkotlin/app/src/main/java/com/mallcloud/utils/kotenum.kt
@@ -6,3 +6,27 @@ package com.mallcloud.utils
  * @email xiezhengkun@beyondsoft.com
  * @remark
  */
+//kot的枚举类型
+enum class Color2{
+    RED,BLACK,BLUE,GREEN,WHITE
+}
+
+//枚举类的初始化
+enum class Color(val rgb: Int) {
+    RED(0xFF0000),
+    GREEN(0x00FF00),
+    BLUE(0x0000FF)
+}
+
+//枚举还支持以声明自己的匿名类及相应的方法、以及覆盖基类的方法。如：
+enum class ProtocolState {
+    WAITING {
+        override fun signal() = TALKING
+    },
+
+    TALKING {
+        override fun signal() = WAITING
+    };
+
+    abstract fun signal(): ProtocolState
+}
--- androidkotlin/app/src/main/java/com/mallcloud/utils/kotextendfun.kt
+++ androidkotlin/app/src/main/java/com/mallcloud/utils/kotextendfun.kt
@@ -6,3 +6,33 @@ package com.mallcloud.utils
  * @email xiezhengkun@beyondsoft.com
  * @remark
  */
+//扩展类，之后还可以 扩展函数
+//扩展函数定义形式：
+//
+//fun receiverType.functionName(params){
+//    body
+//}
+//receiver  函数的接收者
+//函数名   参数
+
+class User (var name:String)
+//扩展函数，可以调用 接收者的类的 属性
+fun User.Prints(){
+    print("用户名 $name")
+}
+//调用也是 对象.扩展函数
+//支持传参，有返回值
+
+//为已存在的类，在不修改内部结构的情况下，扩展函数
+
+//伴生对象的扩展 todo
+class MyClassA{
+    companion object{
+        //伴生对象内部，理解为静态类
+    }
+}
+
+//伴生对象的扩展函数
+fun MyClassA.Companion.foo(){
+    println("伴生对象的扩展函数")
+}
